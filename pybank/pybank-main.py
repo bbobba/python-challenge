7220ae73f0841fd4d98fb0228d87f30e80a2ed45
Becky Bobba <becky.h.bobba@gmail.com>
2018-12-27 09:52:55 -0800

    committing changes to pybank script

--- pybank/pybank-main.py
+++ pybank/pybank-main.py
@@ -8,9 +8,16 @@ import csv
 # Set file path
 csvpath = os.path.join('data', 'budget_data.csv')
 
-# Declare variables
+# Declare variables and empty lists
 
 
 # Read in the csv file
 with open(csvpath, newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
+    #skip header
+
+# Print results to the terminal
+
+
+# Save results in a .txt file
+
