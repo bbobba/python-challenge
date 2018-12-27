5b21d71bb6a85cf3f45b52993951ce4dba5d5fb0
Becky Bobba <becky.h.bobba@gmail.com>
2018-12-27 10:30:07 -0800

    adding pybank instructions to script

--- pybank/pybank-main.py
+++ pybank/pybank-main.py
@@ -1,5 +1,31 @@
-# Python Challenge: PyBank
+
+# PYTHON CHALLENGE: PyBank
+
+
 # -----------------------------------------------------------------
+# INSTRUCTIONS
+
+# Goal:
+#   Create a Python script for analyzing company financial records. See GitHub for details: https://github.com/bbobba/UTAUS201810DATA2/tree/master/03_Python/Homework/Instructions
+# 
+# Dataset: 
+#   Company financial data stored in a csv called budget_data.csv. 
+#   The dataset is composed of two columns: Date and Profit/Losses.
+
+# Calculate:
+#   The total number of months included in the dataset
+#   The total net amount of "Profit/Losses" over the entire period
+#   The average change in "Profit/Losses" between months over the entire period
+#   The greatest increase in profits (date and amount) over the entire period
+#   The greatest decrease in losses (date and amount) over the entire period
+
+# Deliverables:
+#   Print the analysis to the terminal
+#   Export a text file with the results
+
+
+# -----------------------------------------------------------------
+# ANALYSIS
 
 # Import the necessary dependencies for os.path.join()
 import os
@@ -8,7 +34,7 @@ import csv
 # Set file path
 csvpath = os.path.join('data', 'budget_data.csv')
 
-# Declare variables and empty lists
+# Declare variables
 
 
 # Read in the csv file
@@ -16,8 +42,19 @@ with open(csvpath, newline = '') as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ',')
     #skip header
 
-# Print results to the terminal
 
+# -----------------------------------------------------------------
+# OUTPUT
 
-# Save results in a .txt file
+# Print results to the terminal:
+
+#Financial Analysis
+#----------------------------
+#Total Months:
+#Total:
+#Average  Change:
+#Greatest Increase in Profits:
+#Greatest Decrease in Profits:
+
+# Export results in a .txt file:
 
