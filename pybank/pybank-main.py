822b362ba14a712391cf6022544247690cdbcd23
Becky Bobba <becky.h.bobba@gmail.com>
2018-12-30 10:43:25 -0800

    adding updates to pybank code

--- pybank/pybank-main.py
+++ pybank/pybank-main.py
@@ -30,17 +30,25 @@
 import os
 import csv
 
-# Set file path
-csvpath = os.path.join('data', 'budget_data.csv')
-
 # Declare variables
+# empty lists
+total_months = []
+net_revenue = []
+avg_change = []
+max_profit_date = []
+min_profit_date = []
+max_profit_amount = []
+min_profit_amount = []
 
+# Set file path
+csv_path = os.path.join('data', 'budget_data.csv')
 
-# Read in the csv file
-with open(csvpath, newline = '') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter = ',')
-    #skip header
+# Open and read the csv file
+with open(csv_path, newline = '') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter = ',')
 
+    # Skip header for analysis: next(csv_reader, None)
+# 
 
 # -----------------------------------------------------------------
 # OUTPUT
@@ -51,9 +59,10 @@ with open(csvpath, newline = '') as csvfile:
 #----------------------------
 #Total Months:
 #Total:
-#Average  Change:
+#Average Change:
 #Greatest Increase in Profits:
 #Greatest Decrease in Profits:
 
 # Export results in a .txt file:
+# SEE 2>Activities>09-Ins_WriteCSV
 
