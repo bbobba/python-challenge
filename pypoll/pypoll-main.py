307c4f3d8940abf333ef4dd6d56162027b5224ca
Becky Bobba <becky.h.bobba@gmail.com>
2019-02-23 07:34:29 -0600

    committing final solutions

--- pypoll/pypoll-main.py
+++ pypoll/pypoll-main.py
@@ -24,32 +24,94 @@
 #   Export a text file with the results
 
 
-# -----------------------------------------------------------------
-# ANALYSIS
-# See 3>Activities>08-Par_Wrestling>Solved
+# # -----------------------------------------------------------------
+# # ANALYSIS
 
 # Import the necessary dependencies for os.path.join()
 import os
 import csv
 
-# Set file path
-csv_path = os.path.join('data', 'election_data.csv')
+# Set csv path for data file and store in a variable
+csv_path = os.path.join('data','election_data.csv')
 
-# Declare variables
-# empty lists
+# Set txt path for output file and store in a variable
+output_file = os.path.join('election_analysis.txt')
 
+# Create empty lists
+candidate_name = []
+unique_candidates = []
+candidate_votes = []
+candidate_percentage = []
+
+# Counter
+total_votes = 0
 
 # Open and read the csv file
-with open(csv_path, newline = '') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter = ',')
-    # Skip header
-    csv_header = (csv_reader, None)
+with open(csv_path, newline='') as csv_file:
 
-# -----------------------------------------------------------------
-# OUTPUT
+     # Store contents in a variable called csv_reader
+    csv_reader = csv.reader(csv_file, delimiter=',') 
 
-# Print results to the terminal:
+    # Read header
+    header = next(csv_reader)
+
+    # Iterate through the rows
+    for row in csv_reader:
+        
+        # Append candidate names to new list
+        candidate_name.append(row[2])
+        
+        # Tally votes
+        total_votes = total_votes + 1
+
+# Find unique candidates who recieved votes and store in a list
+for candidate in candidate_name:
+    if candidate not in unique_candidates:
+        unique_candidates.append(candidate)
 
 
-# Export results in a .txt file:
 
+# # -----------------------------------------------------------------
+# # OUTPUT
+with open(output_file,'w') as txt_file:
+    
+    # Print election summary to the terminal
+    election_summary = (f'\n\
+    Election Results\n\
+    ----------------------------\n\
+    Total Votes: {total_votes}\n\
+    ----------------------------')
+    print(election_summary)
+    
+    # Save election summary in the text file
+    txt_file.write(election_summary)
+    
+    # Calculate the percentage of votes and total number of votes each candidate won
+    for i in range(0, len(unique_candidates)):
+        candidate_votes.append(candidate_name.count(unique_candidates[i]))
+        candidate_percentage.append(candidate_name.count(unique_candidates[i])/total_votes*100)
+        candidate_summary = f'{unique_candidates[i]}: {candidate_percentage[i]:0.3f}% ({candidate_votes[i]})\n'
+        
+        # Print candidate summary to the terminal
+        print(candidate_summary, end='')
+        
+        # Save candidate summary in the text file
+        txt_file.write(candidate_summary)
+
+    # Find the winner of the election based on popular vote
+    winner_votes = max(candidate_votes)
+    winner_index = candidate_votes.index(winner_votes)
+    winner_name = unique_candidates[winner_index]
+
+    # Print winner summary to the terminal
+    winner_summary = (f'\n\
+    ----------------------------\n\
+    Winner: {winner_name}\n\
+    ----------------------------')
+    print(winner_summary)
+    
+    # Save winner summary in the text file
+    txt_file.write(winner_summary)
+
+
+    
